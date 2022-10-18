ce03dfdcee125ecf15b4d765ce1807ccdc1c547d
DarkStyleee <ussaevf@gmail.com>
2022-10-18 16:38:13 +0300

    decomposed schedule events from MainView to TTBlockSchedule component

--- src/components/TTBlockSchedule.vue
+++ src/components/TTBlockSchedule.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="block-table">
-    <img src="@/assets/images/table.png" alt="table" />
+  <div class="schedule">
+    <VueCal
+      locale="ru"
+      active-view="week"
+      :disable-views="['years', 'year', 'month']"
+      style="height: 500px"
+      :events="events"
+      :time-from="8 * 60"
+      :time-to="22 * 60"
+    ></VueCal>
   </div>
 </template>
 
 <script>
-export default {};
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+export default {
+  components: {
+    VueCal,
+  },
+  props: {
+    events: Object,
+  },
+};
 </script>
 
 <style></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -23,19 +23,7 @@
       titleName="Расписание тренировок"
     ></TTBlockTitle>
     <div class="block-4">
-      <div class="schedule">
-        <VueCal
-          locale="ru"
-          active-view="week"
-          :disable-views="['years', 'year', 'month']"
-          style="height: 500px"
-          :events="scheduleEvents"
-          :time-from="8 * 60"
-          :time-to="22 * 60"
-        ></VueCal>
-      </div>
-
-      <!-- <TTBlockSchedule></TTBlockSchedule> -->
+      <TTBlockSchedule :events="scheduleEvents"></TTBlockSchedule>
     </div>
     <TTBlockFooter></TTBlockFooter>
   </div>
@@ -45,7 +33,7 @@
 import TTBlockHeader from "@/components/TTBlockHeader.vue";
 import TTBlockMain from "@/components/TTBlockMain.vue";
 import TTBlockFooter from "@/components/TTBlockFooter.vue";
-// import TTBlockSchedule from "@/components/TTBlockSchedule.vue";
+import TTBlockSchedule from "@/components/TTBlockSchedule.vue";
 import TTBlockTitle from "@/components/TTBlockTitle.vue";
 import TTCardProgram from "@/components/TTCardProgram.vue";
 import TTCardTrainer from "@/components/TTCardTrainer.vue";
@@ -53,27 +41,21 @@ import TTCardTrainer from "@/components/TTCardTrainer.vue";
 import { computed } from "vue";
 import { useStore } from "vuex";
 
-import VueCal from "vue-cal";
-import "vue-cal/dist/vuecal.css";
-
 export default {
   components: {
     TTBlockHeader,
     TTBlockMain,
     TTBlockFooter,
-    // TTBlockSchedule,
+    TTBlockSchedule,
     TTBlockTitle,
     TTCardProgram,
     TTCardTrainer,
-    VueCal,
   },
   setup() {
     const store = useStore();
     const programs = computed(() => store.getters.getProgramCards);
     const trainers = computed(() => store.getters.getTrainerCards);
     const scheduleEvents = computed(() => store.getters.getScheduleEvents);
-
-    // console.log(programs.value);
 
     return {
       programs,
